e96b1781824d484f1f4cbf71b70566c0b744520b
Leo <leonid112@yandex.ru>
2025-12-01 10:47:07 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,115 @@
+'''
+RPG - магазин и инвентарь
+'''
+import sys
+from pathlib import Path
+from PyQt6.QtWidgets import QApplication, QWidget, \
+QListWidget, QGridLayout, QPushButton, QComboBox, QLabel
+import pymysql.cursors
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.items_id_lst = []
+        self.showMaximized()
+        self.setWindowTitle("САМАЯ ЛУЧШАЯ РОЛЁВКА")
+        layout = QGridLayout()
+        self.setLayout(layout)
+        self.db_connect()
+
+        layout.addWidget(QLabel('Инвентарь'),0,0)
+        layout.addWidget(QLabel('Добавление в инвентарь'),0,1)
+        layout.addWidget(QLabel('Магазин'),0,2,1,2)
+
+        self.inventory_lst = QListWidget()
+        self.inventory_lst.setFixedWidth(500)
+        self.shop_lst = QListWidget()
+        self.shop_lst.setFixedWidth(500)
+        self.sell_btn = QPushButton("Продать")
+        self.sell_btn.clicked.connect(self.sel_item)
+        self.buy_btn = QPushButton("Купить")
+        self.buy_btn.clicked.connect(self.buy_item)
+        self.all_items_lst = QComboBox()
+        self.all_items_lst.activated.connect(self.add_inventory)
+
+        layout.addWidget(self.inventory_lst,1,0)
+        layout.addWidget(self.shop_lst,1,2,1,2)
+        layout.addWidget(self.sell_btn,2,2)
+        layout.addWidget(self.buy_btn,2,3)
+        layout.addWidget(self.all_items_lst,1,1)
+        self.get_shop()
+        self.get_all_items()
+        self.show()
+
+    def db_connect(self):
+        self.connection = pymysql.connect(
+            host='localhost',
+            user='pk31',
+            password='1234',
+            database='pk31_rpg'
+        )
+        self.cursor = self.connection.cursor()
+
+    def get_shop(self):
+        sql = '''SELECT `вещи`.`название`, `вещи`.`редкость`,
+                    `магазин`.`количество`,`магазин`.`цена_покупки`, 
+                    `магазин`.`цена продажи` 
+                    FROM `магазин` 
+                    INNER JOIN `вещи`
+                    ON `вещи`.`id` = `магазин`.`id_вещи`'''
+        self.cursor.execute(sql)
+        ans = self.cursor.fetchone()
+        self.shop_lst.clear()
+        while (ans):
+            txt = f'{ans[0]} {ans[1]} {ans[2]} {ans[3]} {ans[4]}'
+            self.shop_lst.addItem(txt)
+            ans = self.cursor.fetchone()
+    
+    def get_all_items(self):
+        sql = 'SELECT * FROM `Вещи`'
+        self.cursor.execute(sql)
+        ans = self.cursor.fetchone()
+        self.all_items_lst.clear()
+        while (ans):
+            txt = f'{ans[1]} {ans[2]} {ans[3]}'
+            self.items_id_lst.append(ans[0])
+            self.all_items_lst.addItem(txt)
+            ans = self.cursor.fetchone()
+            #id выбранной вещи лежит в отдельном списке
+
+    def add_inventory(self):
+        selected_number = self.all_items_lst.currentIndex()#это номер из списка id!!!
+        selected_id = self.items_id_lst[selected_number]
+        sql = 'INSERT INTO `Инвентарь`(`id_вещи`, `Количество`) VALUES(%s,1)'
+        self.cursor.execute(sql, (selected_id,))
+        self.connection.commit()
+        self.show_inventory()
+
+
+    def show_inventory(self):
+        self.inventory_lst.clear()
+        sql = '''SELECT `Вещи`.`название`, `Вещи`.`цена`, `Вещи`.`редкость`,
+        `Инвентарь`.`количество`
+        FROM `Инвентарь`
+        INNER JOIN `Вещи`
+        ON `Инвентарь`.`Id_вещи` = `Вещи`.`id`;'''
+        self.cursor.execute(sql)
+        ans = self.cursor.fetchone()
+        while ans:
+            self.inventory_lst.addItem(f'{ans[0]} {ans[1]} {ans[2]} {ans[3]}')
+            ans = self.cursor.fetchone()
+
+    def sel_item(self):
+        if self.inventory_lst.currentIndex() is not None:
+            print('можно продать')
+
+    def buy_item(self):
+        if self.shop_lst.currentIndex() is not None:
+            print("можно купить")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet(Path('login.qss').read_text(encoding='utf-8'))
+    window = MainWindow()
+    sys.exit(app.exec())
